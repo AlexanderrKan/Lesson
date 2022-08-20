7c3771bbdf83c05cbb4e2833cabcd909e6c1a075
AlexanderrKan <110777901+AlexanderrKan@users.noreply.github.com>
2022-08-20 20:40:43 +0300

    Add files via upload

---
+++ Sumofnumbers.java
@@ -0,0 +1,11 @@
+public class Sumofnumbers {
+    public static void main(String[] agrs) {
+        {
+            int a = 10;
+            int b = 15;
+            int c = 20;
+            int d = a + b + c;
+            System.out.println("Sum of numbers: " + d);
+        }
+    }
+}
