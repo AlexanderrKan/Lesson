bda88cfafd8aec7585e51ecd0324926d2655ff41
AlexanderrKan <110777901+AlexanderrKan@users.noreply.github.com>
2022-08-22 17:47:46 +0300

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,15 @@
+public class MultiplicationTable {
+    public static void main(String args[]){
+        for (int i = 1; i<10; i++){
+            for (int j = 1; j<10; j++){
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+
+        for (int a = 1; a<8; a++) {
+            for (int b = 7; b < 8; b++) {
+                System.out.println(a + "*" + b + "=" + a * b);
+            }
+        }
+    }
+}
