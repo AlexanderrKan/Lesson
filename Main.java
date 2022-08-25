80fd4d604903a08f72dd522ed5f338f14d671085
AlexanderrKan <110777901+AlexanderrKan@users.noreply.github.com>
2022-08-25 14:54:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String [] args) {
+        Human man = new Human (36, "Ivan");
+        man.talk(man.Fullname);
+        man.move(man.age);
+        }
+}
