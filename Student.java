ba456313f7eb4e5bbe67ba99afe1f409e052b68d
AlexanderrKan <110777901+AlexanderrKan@users.noreply.github.com>
2022-08-26 16:40:57 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,16 +2,21 @@ public class Student {
     String Firstname;
     String Lastname;
     int group;
-    double averageMark;
+    int averageMark;
 
-    public Student (String Ivan, String Ivanov, int group, double averageMark) {
+    public Student (String Ivan, String Ivanov, int 802, int 5) {
         this.Firstname = Ivan;
         this.Lastname = Ivanov;
         this.group = 802;
         this.averageMark = 5;
+
+        if (averageMark == 5) {
+            System.out.println("сумма 100 р.");
+        }
+        else {
+            System.out.printf("сумма 80 р.");
+        }
+
     }
-
     double getScholarship() {return this.averageMark;}
-
 }
-
