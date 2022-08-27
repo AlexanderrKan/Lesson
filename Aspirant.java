d46009786f825b8cf617f7d16f68452b624444bb
AlexanderrKan <110777901+AlexanderrKan@users.noreply.github.com>
2022-08-27 16:16:41 +0300

    Update Aspirant.java

--- Aspirant.java
+++ Aspirant.java
@@ -1,22 +1,29 @@
 public class Aspirant extends Student {
     String Science;
 
-    public Aspirant (String Ivan, String Ivanov, int group, int averageMark, String Astrology){
+    public Aspirant (String Ivan, String Ivanov, int group, double averageMark, String Astrology){
     super(Ivan, Ivanov, group, averageMark);
         this.Science = Astrology;
+
     }
+
+    public Aspirant() {
+        
+    }
+
     public void main(String[] args){
+        Student student = new Student();
+        Aspirant aspirant = new Aspirant();
         Student.getScholarship();
         Aspirant.getScholarship();
     }
-    @Override
-    static void getScholarship() {
-        if (averageMark == 5) {
-            System.out.println("сумма 200 р.");
-        } else {
-            System.out.printf("сумма 180 р.");
-        }
+    @Override // 
+        void getScholarship() {
+            if (averageMark == 5) {
+                System.out.println("сумма 200 р.");
+            } else {
+                System.out.println("сумма 180 р.");
+            }
     }
+
 }
-
-
