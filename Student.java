28cb8577378ba5f60cbfde32104acba4faac6c0d
AlexanderrKan <110777901+AlexanderrKan@users.noreply.github.com>
2022-08-27 09:15:37 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -4,19 +4,21 @@ public class Student {
     int group;
     int averageMark;
 
-    public Student (String Ivan, String Ivanov, int 802, int 5) {
+    public Student(String Ivan, String Ivanov, int 802, int 5) {
         this.Firstname = Ivan;
         this.Lastname = Ivanov;
         this.group = 802;
         this.averageMark = 5;
 
+
+    }
+
+    double getScholarship() {
         if (averageMark == 5) {
             System.out.println("сумма 100 р.");
-        }
-        else {
+        } else {
             System.out.printf("сумма 80 р.");
         }
 
     }
-    double getScholarship() {return this.averageMark;}
 }
